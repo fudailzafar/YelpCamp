322ed80bb331d0865c5af6e75412f32a8ae3ca4e
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-12-22 13:48:22 +0530

    OOPs for error handling

---
+++ utils/ExpressError.js
@@ -0,0 +1,9 @@
+class ExpressError extends Error {
+    constructor(message, statusCode) {
+        super();
+        this.message = message;
+        this.statuscode = statusCode;
+    }
+}
+
+module.exports = ExpressError;
