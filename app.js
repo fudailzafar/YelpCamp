b99fe00bc89ff64136d49404de645c20ea9f8a47
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-11 19:04:50 +0530

    Displaying Reviews

--- app.js
+++ app.js
@@ -69,10 +69,10 @@ app.post(
   })
 );
 
-app.get("/campgrounds/:id", async (req, res) => {
-  const campground = await Campground.findById(req.params.id);
-  res.render("campgrounds/show", { campground });
-});
+app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
+  const campground = await Campground.findById(req.params.id).populate('reviews');
+  res.render('campgrounds/show', { campground });
+}));
 
 app.get(
   "/campgrounds/:id/edit",
