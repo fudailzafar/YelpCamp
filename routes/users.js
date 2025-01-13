480875551943c59d108a2cea1b50bdc4f4cd2cfd
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-13 19:24:50 +0530

    Backend functionality
    adding logout,  currentUser Helper & fixing router route.

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,16 @@ const User = require('../models/user');
 router.get('/register', (req, res) => {
     res.render('users/register');
 });
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        
+        req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success', 'Welcome to Yelp Camp!');
             res.redirect('/campgrounds');
-        
+        })
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
@@ -31,5 +31,15 @@ router.post('/login', passport.authenticate('local', { failureFlash: true, failu
     req.flash('success', 'welcome back!');
     res.redirect('/campgrounds');
 })
+
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 module.exports = router;
