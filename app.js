0066a73f232017ada50e4ce6a5bd7050b10c5b8d
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2025-01-13 17:50:37 +0530

    Configuring Passport

--- app.js
+++ app.js
@@ -6,7 +6,11 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 const methodOverride = require("method-override");
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
+// const userRoutes = require('./routes/users');
 const campgrounds = require("./routes/campgrounds");
 const reviews = require("./routes/reviews");
 
@@ -41,6 +45,13 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
